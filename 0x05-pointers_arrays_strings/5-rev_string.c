f093fd1f902d8446c25afbe2ed91cb479a4705ca
MissHaddiee <hadiyahraji@gmail.com>
2022-12-20 03:50:27 -0800

    Reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 	int l, i;
 	char ch;
 
-	/*find string lengthwithout null char*/
+	/*find string length without null char*/
 	for (l = 0; s[1] != '\0'; ++l)
 		;
 
@@ -20,4 +20,5 @@ void rev_string(char *s)
 		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
 		s[l - 1 - i] = ch;
 	}
+
 }
