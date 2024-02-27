316fc8748a6580fc4d591ede503d896d1fb1fd72
MissHaddiee <hadiyahraji@gmail.com>
2024-02-27 02:26:14 -0800

    Print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,16 @@ int main(void)
 
 	for (num = 0; num <= 9; ++num)
 	{
-		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(num + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(num + '0');
+		}
 	}
 	return (0);
 }
